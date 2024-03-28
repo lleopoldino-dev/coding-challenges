96e720a99124d876479747a49c63dd208b51b4be
lleopoldino <lleopoldino@softjournua.onmicrosoft.com>
2024-03-28 10:03:25 -0300

    add selection sort algorithm

---
+++ Coding.Challenges.Algorithms/Selection-Sort/README.md
@@ -0,0 +1,24 @@
+# Selection Sort
+
+The algorithm repeatedly selects the smallest (or largest) element from the unsorted portion of the list and swaps it with the first element of the unsorted part. This process is repeated for the remaining unsorted portion until the entire list is sorted.
+
+Input format:
+
+- The first line contains integer n the size of the array.
+- Each of the next n lines contains an integer with value from array.
+
+Input sample:
+```
+3
+12
+44
+7
+```
+
+
+Output:
+```
+7, 12, 44
+```
+
+For more information, consult [GeeksForGeeks](https://www.geeksforgeeks.org/selection-sort/).
---
+++ Coding.Challenges.Algorithms/Selection-Sort/SelectionSort.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Coding.Challenges.Algorithms.Selection_Sort;
+
+public class SelectionSort
+{
+    public static void Execute(List<int> list, bool recursive = false)
+    {
+        if (recursive)
+        {
+            ExecuteSelectionSort(list);
+        }
+        else
+        {
+            ExecuteSelectionSortRecursive(list, list.Count, 0);
+        }
+
+
+        Console.WriteLine(string.Join(", ", list));
+    }
+
+    public static void ExecuteSelectionSort(List<int> list)
+    {
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            var minIndex = i;
+
+            for (int j = i + 1; j < list.Count - 1; j++)
+            {
+                if (list[j] < list[minIndex]) 
+                { 
+                    minIndex = j;
+                }
+            }
+
+            var temp = list[i];
+            list[minIndex] = list[i];
+            list[i] = temp;
+        }
+    }
+
+    public static void ExecuteSelectionSortRecursive(List<int> list, int arrLength, int startIndex)
+    {
+        if(arrLength == startIndex)
+        {
+            return;
+        }
+
+        var minIndex = MinIndex(list, startIndex, arrLength - 1);
+
+        if (minIndex != startIndex)
+        {
+            var temp = list[minIndex];
+            list[minIndex] = list[startIndex];
+            list[startIndex] = temp;
+        }
+        
+        ExecuteSelectionSortRecursive(list, arrLength, startIndex + 1);
+    }
+
+    private static int MinIndex(List<int> list, int index, int arraySize)
+    {
+        if (index == arraySize)
+        {
+            return index;
+        }
+
+        int minIndex = MinIndex(list, index + 1, arraySize);
+
+        return list[index] < list[minIndex] ? index : minIndex;
+    }
+}
